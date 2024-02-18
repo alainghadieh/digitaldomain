41d0f18f8c229092b7fcf18310bcdd855c2b95fa
Alain Ghadieh <alain.g@scopicsoftware.com>
2024-02-18 20:15:37 +0200

    refactor code
    by doing abstractions (creating components), added font, and changed meta title and description

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.{html}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Big Shoulders Display"', 'sans-serif'],
+    },
+    extend: {
+      textColor: {
+        'primary': '#91d5c1',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+      },
+    },
   },
   plugins: [],
 }
