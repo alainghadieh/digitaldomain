cda7c4c22db653b10f1cb17c10c180eed827f6fd
Alain Ghadieh <alain.g@scopicsoftware.com>
2024-02-27 15:35:08 +0200

    safely remove the background polygons
    to help with page load

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import {BackgroundPolygon1} from "./components/BackgroundPolygon1";
-import {BackgroundPolygon2} from "./components/BackgroundPolygon2";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Home } from "./views/Home";
@@ -16,7 +14,6 @@ export default function App() {
     <>
       <Header></Header>
       <div className="relative isolate px-6 pt-14 lg:px-8 flex justify-center">
-        <BackgroundPolygon1 />
         <div className="mx-auto max-w-screen-lg lg:py-12 sm:py-6 ">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -25,7 +22,6 @@ export default function App() {
             <Route path="/portfolio" element={<Portfolio />} />
           </Routes>
         </div>
-        <BackgroundPolygon2 />
       </div>
       <Footer />
     </>
